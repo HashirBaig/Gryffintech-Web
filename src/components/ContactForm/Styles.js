800a14298c8fcb7ccbc964bb31a8f978efb60f58
Hashir Baig <hashirbaig456@gmail.com>
2022-10-06 14:41:35 +0500

    initial commit

--- src/components/ContactForm/Styles.js
+++ src/components/ContactForm/Styles.js
@@ -1,5 +1,40 @@
 import styled from "styled-components"
+import { colors, font } from "../../const"
+import { Form, Field } from "formik"
 
-export const StyledWrapper = styled.div`
-  margin: 20px 0px;
+export const StyledLabel = styled.label`
+  color: ${colors.darker};
+`
+export const StyledForm = styled(Form)``
+
+export const StyledField = styled(Field)`
+  width: 100%;
+  height: 40px;
+  border: 2px solid ${colors.darker};
+  padding: 0 10px;
+  border-radius: 4px;
+  box-shadow: none;
+  outline: none;
+
+  font-size: ${font.size.lg};
+`
+
+export const StyledButtonContainer = styled.div``
+export const StyledButton = styled.button`
+  outline: none;
+  font-size: 16px;
+  font-weight: 600;
+  padding: 10px 25px;
+  border: 2px solid ${colors.dark};
+  margin: 15px 0px;
+  border-radius: 4px;
+  color: ${colors.light};
+  background-color: ${colors.dark};
+  cursor: pointer;
+  transition: all 0.3ms ease;
+  :hover {
+    border: 2px solid ${colors.dark};
+    background-color: ${colors.light};
+    color: ${colors.dark};
+  }
 `
--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -4,22 +4,19 @@ import { StyledWrapper } from "./Styles"
 import Header from "../../components/layouts/home"
 import Carousel from "../../components/Carousel"
 import Services from "../../components/Services"
-import ContactForm from "../../components/ContactForm"
+import Contact from "../../components/Contact"
 
 function Landing() {
   return (
     <StyledWrapper>
       {/*
-      - Header (Done)
-      - Caseroul
-      - Service Cards
       - Contact us form
       - Footer
     */}
       <Header />
       <Carousel />
       <Services />
-      <ContactForm />
+      <Contact />
     </StyledWrapper>
   )
 }
